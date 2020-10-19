d3fbdae6ca8378b0faa9441bd7bef8b8566b31f5
Rafael Oliveira <rafaelcardosooliveira0@gmail.com>
2020-10-19 22:08:26 +0100

    tiny fix on challenge #2

--- 2_longest_substring_without_repeating_characters/main.py
+++ 2_longest_substring_without_repeating_characters/main.py
@@ -11,15 +11,17 @@ class Solution:
         chars_list = []
 
         # loop char by char
-        for c in s:
+        for i, c in enumerate(s):
             if c not in chars_list:
                 chars_list.append(c)
                 tmp += 1
+
+                if tmp >= longest_number:
+                    longest_number = tmp
             else:
                 # we found a similar
                 if tmp >= longest_number:
                     longest_number = tmp
-                    print(chars_list)
 
                 chars_list.clear()
 
@@ -27,7 +29,16 @@ class Solution:
                 tmp = 1
                 chars_list.append(c)
 
+                # backpropagation, it is used to fetch some non-repetitive characters that may be in the back
+                for j in range(i - 1, 0, -1):
+                    back_character = s[j]
+                    if back_character != c:
+                        tmp += 1
+                        chars_list.append(back_character)
+                    else:
+                        break
+
         return longest_number
 
 
-print(Solution().lengthOfLongestSubstring('abrkaabcdaefghijjxxx'))
+print(Solution().lengthOfLongestSubstring('abcabcbb'))
