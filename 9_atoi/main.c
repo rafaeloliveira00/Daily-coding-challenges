5a6224fbb3afdf78890e18ce2e9cb288b8f8d41b
Rafael Oliveira <rafaelcardosooliveira0@gmail.com>
2020-11-11 10:51:36 +0000

    fixed challenge #9

--- 9_atoi/main.c
+++ 9_atoi/main.c
@@ -1,59 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+
 
 int myAtoi(char *s) {
 
-    char final[50];
-    int found_valid_numbers = 0;
-
+    long long final = 0;
     int is_negative = 0;
     // if true then we found a +, - or an number already
-    int start_to_save = 0;
+    int start_to_process = 0;
     for (int i = 0; s[i] != '\0'; i++) {
         if (s[i] == ' ') {
-            if (start_to_save == 1)
+            if (start_to_process)
                 break;
         } else if (s[i] == '+') {
-            if (start_to_save == 1)
+            if (start_to_process)
                 break;
 
-            start_to_save = 1;
+            start_to_process = 1;
         } else if (s[i] == '-') {
-            if (start_to_save == 1)
+            if (start_to_process)
                 break;
 
-            start_to_save = 1;
+            start_to_process = 1;
             is_negative = 1;
         } else if (s[i] >= 48 && s[i] <= 57) {
-            start_to_save = 1;
+            start_to_process = 1;
 
-            // if it is an number, add it to the new string
-            final[found_valid_numbers] = s[i];
-            found_valid_numbers++;
+            // multiplying by 10, will shift the digits to the left
+            final = final * 10 + s[i] - '0';
+            
+            if (is_negative && final * -1 < -pow(2, 31))
+                return (int) -pow(2, 31);
+
+            else if (!is_negative && final >= pow(2, 31) - 1)
+                return (int) pow(2, 31);
 
         } else
             break;
     }
 
-    // if the current position is 0, then we do not found a valid digit
-    if (found_valid_numbers == 0)
-        return 0;
-
-    // convert the clean string to number
-    int final_int = atoi(final);
     // convert to negative if has negative
     if (is_negative == 1)
-        final_int *= -1;
+        final *= -1;
 
-    // cap the result
-    if (final_int < -pow(2, 31))
-        return (int) -pow(2, 31);
-
-    else if (final_int >= pow(2, 31))
-        return (int) pow(2, 31) - 1;
-
-    return final_int;
+    return (int) final;
 }
 
 int main(void) {
